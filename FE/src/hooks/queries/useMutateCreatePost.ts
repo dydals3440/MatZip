d749f642ead4e21f45c187092d1fd3cce2f8c952
김용민 <dydals3440@gmail.com>
2024-05-05 02:42:24 +0900

    fix: longitutde 철자 변경

--- FE/src/hooks/queries/useMutateCreatePost.ts
+++ FE/src/hooks/queries/useMutateCreatePost.ts
@@ -8,6 +8,7 @@ import {useMutation} from '@tanstack/react-query';
 function useMutationCreatePost(mutationOptions?: UseMutationCustomOptions) {
   return useMutation({
     mutationFn: createPost,
+    // createPost가 성공했을 떄 성공한 데이터를 줌. 그 데이터를 newMarker에 담아서, 추가해주면 됨.
     onSuccess: newPost => {
       // 1. 업데이트: 쿼리를 무효화
       // queryClient.invalidateQueries({
@@ -15,18 +16,19 @@ function useMutationCreatePost(mutationOptions?: UseMutationCustomOptions) {
       // });
       // console.log('newPost', newPost);
       // 2. 업데이트: 캐시를 직접 업데이트 (setQueryData이용)
+      // 2번째 인자에 쿼리키에 해당하는 데이터를 모두 받아볼 수 있음.
+      // 새로운 마커를 여기다 넣어주면됨. 새로운 데이터는 onSuccess에서 받아옴.
       queryClient.setQueryData<Marker[]>(
         [queryKeys.MARKER, queryKeys.GET_MARKERS],
-        // 2번째 인자에 쿼리키에 해당하는 데이터를 받아볼 수 있음.
         existingMarkers => {
           const newMarker = {
             id: newPost.id,
             latitude: newPost.latitude,
-            longitutde: newPost.longitude,
+            longitude: newPost.longitude,
             color: newPost.color,
             score: newPost.score,
           };
-
+          // 맨처음 등록시는 newMarker만 넣어주면 됨.
           return existingMarkers
             ? [...existingMarkers, newMarker]
             : [newMarker];
